2bfce6f95db88cf966a9aad8324b45944ea0a4fb
ravi <samcollins9899@gmail.com>
2023-09-15 01:54:02 +0530

    added api fix

--- server.js
+++ server.js
@@ -50,12 +50,19 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.use(cors({
-    origin: "https://scribly-note.vercel.app",
-    methods: "GET,POST,PUT,DELETE,PATCH",
-    credentials: true,
-  })
-);
+// app.use(cors({
+//     origin: "https://scribly-note.vercel.app",
+//     methods: "GET,POST,PUT,DELETE,PATCH",
+//     credentials: true,
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://scribly-note.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 //another solution
 // app.use(
