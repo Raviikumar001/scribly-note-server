94ede43e63f32cb7d0aaa2d1cf5433da92800820
ravi <samcollins9899@gmail.com>
2023-09-15 02:11:18 +0530

    added solution

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 // );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://scribly-note.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
