46757ae9b960b56bc97a872b2d4a9e789d8eeda0
ravi <samcollins9899@gmail.com>
2023-09-15 02:14:54 +0530

    added cors

--- server.js
+++ server.js
@@ -57,13 +57,14 @@ app.use(bodyParser.urlencoded({extended:true}))
 //   })
 // );
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// })
 
+app.use(cors())
 //another solution
 // app.use(
 //     session({
