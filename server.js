c82c8b28bca4735850b068af7a048abc37f6a1d1
ravi <samcollins9899@gmail.com>
2023-09-14 18:14:07 +0530

    added origin values

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://scribly-note.vercel.app",
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true,
   })
