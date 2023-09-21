ac3d8c4b71bdae31a369f614f8d9513499867bb6
ravi <samcollins9899@gmail.com>
2023-09-22 00:21:03 +0530

    added new auth url

--- controllers/google-auth.js
+++ controllers/google-auth.js
@@ -12,7 +12,7 @@ passport.use(
     new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "https://test-auth-7lhs.onrender.com/auth/google/callback",
+        callbackURL: "https://scribly-note.up.railway.app/auth/google/callback",
         scope: ["profile", "email"]
       },
         function(request,accessToken, refreshToken,profile,done){
